c36684008ebee7be80a6bdf55b483bd5044a8491
antorhasan <kaziantorhasan@gmail.com>
2019-07-06 00:01:25 +0600

    img crop

--- img_crop.py
+++ img_crop.py
@@ -3,12 +3,21 @@ import cv2
 img = cv2.imread('../road_trc/dataset/data/imagery/amsterdam_0_0_sat.png', 1)
 #print(im.shape)
 
-for i in range(8):
+""" for i in range(8):
     for j in range(8):
 
         im = img[512*i:512*(i+1),512*j:512*(j+1)]
-        cv2.imwrite('./data/img/amsterdam'+str(i)+str(j)+'.png', im)
 
+       
+        cv2.imwrite('./data/img/amsterdam'+str(i)+str(j)+'.png', im)
+ """
+for i in range(4):
+    for j in range(4):
+
+        im = img[1024*i:1024*(i+1),1024*j:1024*(j+1)]
+
+       
+        cv2.imwrite('./data/img/amsterdam'+str(i)+str(j)+'.png', im)
 
 """ cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.imshow('image',im)

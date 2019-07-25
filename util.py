f80d8972a2245b7b6fdf587064c8622c15a97ba4
antorhasan <kaziantorhasan@gmail.com>
2019-07-25 20:07:18 +0600

    stiching images done

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 from os import listdir
 from os.path import isfile, join
 import cv2
+
+
 def dirtodic(path):
     '''make a dict where the keys are common file name prefixes within a 
     directroy. input : path, output: dict obj of filenames'''
@@ -22,90 +24,100 @@ def dirtodic(path):
     return dic
 
 
-print(len(dirtodic('/media/antor/Stuff/projects/road_net/code/road_trc/dataset/data/imagery')))
-
-var = dirtodic('/media/antor/Stuff/projects/road_net/code/road_trc/dataset/data/imagery')
+#var = dirtodic('/media/antor/Stuff/projects/road_net/code/road_trc/dataset/data/imagery')
 
 def mergeimg(lis):
+    '''given a dict, everyimage from that dict is stiched into super images'''
+    
     img_path = '/media/antor/Stuff/projects/road_net/code/road_trc/dataset/data/imagery/'
 
+    for w in lis.keys() :
 
-    bos = sorted(lis['amsterdam'])
-    #bos = bos[0:31]
-    numlis = []
-    #for i in bos:
-    for i in bos :
-        numbers = i.split('_')
-        numlis.append(numbers[1])
-    numlis = list(dict.fromkeys(numlis))
-    print(numlis) 
+        bos = sorted(lis[w])
+        #bos = bos[0:31]
+        numlis = []
+        #for i in bos:
+        for i in bos :
+            numbers = i.split('_')
+            numlis.append(numbers[1])
+        numlis = list(dict.fromkeys(numlis))
+        
 
-    coun = 0
-    coun_con = 0
-    print(bos)
-    prepre_ls = []
-    for i in bos:
-        splitted = i.split('_')
+        coun = 0
+        coun_con = 0
+        print(bos)
+        print(numlis)
+        prepre_ls = []
+        for i in bos:
+            splitted = i.split('_')
 
-        if coun == 0 :
-            pre_rnum = splitted[1]
-            pre_cnum = splitted[2]
-            pre_img = cv2.imread(img_path + i)
-            #print(pre_rnum, pre_cnum)
-            coun += 1
-            continue
+            if coun == 0 :
+                pre_rnum = splitted[1]
+                pre_cnum = splitted[2]
+                pre_img = cv2.imread(img_path + i)
+                #print(pre_rnum, pre_cnum)
+                coun += 1
+                continue
 
-        new_rnum = splitted[1]
-        new_cnum = splitted[2]
-        new_img = cv2.imread(img_path + i)
-        if int(new_rnum) == int(pre_rnum) :
-            #print(new_cnum,pre_cnum)
-            if int(new_cnum) > int(pre_cnum) :
-                pre_img = cv2.vconcat([pre_img, new_img])
-            else :
-                pre_img = cv2.vconcat([new_img, pre_img])
-            pre_rnum = new_rnum
-            pre_cnum = new_cnum
-            #prepre_num = new_rnum
-            #print(prepre_num)
-            #prepre_ls.append(prepre_num)
-            #print(pre_rnum,pre_cnum)
-        elif int(new_rnum) != int(pre_rnum):
-            if coun_con == 0 :
-                pre_con_img = pre_img
+            new_rnum = splitted[1]
+            new_cnum = splitted[2]
+            new_img = cv2.imread(img_path + i)
+            if int(new_rnum) == int(pre_rnum) :
+                #print(new_cnum,pre_cnum)
+                if int(new_cnum) > int(pre_cnum) :
+                    pre_img = cv2.vconcat([pre_img, new_img])
+                else :
+                    pre_img = cv2.vconcat([new_img, pre_img])
+                pre_rnum = new_rnum
+                pre_cnum = new_cnum
+
+                if i == bos[-1] :
+                    new_con_img = pre_img
+                    #if int(pre_rnum) > int(prepre_num):
+                    print(numlis[coun_con],numlis[coun_con-1])
+                    if int(numlis[coun_con]) > int(numlis[coun_con-1]):
+                        pre_con_img = cv2.hconcat([pre_con_img, new_con_img])
+                    else:
+                        pre_con_img = cv2.hconcat([new_con_img, pre_con_img])
+                #prepre_num = new_rnum
+                #print(prepre_num)
+                #prepre_ls.append(prepre_num)
+                #print(pre_rnum,pre_cnum)
+            elif int(new_rnum) != int(pre_rnum):
+                if coun_con == 0 :
+                    pre_con_img = pre_img
+                    pre_rnum = new_rnum
+                    pre_cnum = new_cnum
+                    pre_img = cv2.imread(img_path + i)
+                    coun_con += 1
+                    continue
+
+                new_con_img = pre_img
+                #if int(pre_rnum) > int(prepre_num):
+                print(numlis[coun_con],numlis[coun_con-1])
+                if int(numlis[coun_con]) > int(numlis[coun_con-1]):
+                    pre_con_img = cv2.hconcat([pre_con_img, new_con_img])
+                
+                else:
+                    pre_con_img = cv2.hconcat([new_con_img, pre_con_img])
+                    
                 pre_rnum = new_rnum
                 pre_cnum = new_cnum
                 pre_img = cv2.imread(img_path + i)
                 coun_con += 1
-                continue
-
-            new_con_img = pre_img
-            #if int(pre_rnum) > int(prepre_num):
-            print(numlis[coun_con],numlis[coun_con-1])
-            if int(numlis[coun_con]) > int(numlis[coun_con-1]):
-                pre_con_img = cv2.hconcat([pre_con_img, new_con_img])
-               
-            else:
-                pre_con_img = cv2.hconcat([new_con_img, pre_con_img])
                 
-            pre_rnum = new_rnum
-            pre_cnum = new_cnum
-            pre_img = cv2.imread(img_path + i)
-            coun_con += 1
-            
-        coun += 1
+            coun += 1
 
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    cv2.imshow('image',pre_con_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows() 
+        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+        cv2.imshow('image',pre_con_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows() 
 
-    cv2.imwrite('./data/superimg/amsterdam'+'.png', pre_con_img)
+        cv2.imwrite('./data/superimg/'+ w +'.png', pre_con_img)
         
-        
-    return lis
 
-mergeimg(var)
+
+
 
 def gph_crop(nodes, edges):
     "crops the graph to fit the image"
